1284df084efe19bb85030467f8bce5779e8c87b9
Kristine <s224197@dtu.dk>
2024-01-11 14:11:17 +0100

    Navne

--- ML_prepare.py
+++ ML_prepare.py
@@ -5,29 +5,29 @@ from sklearn.dummy import DummyClassifier
 from sklearn.tree import DecisionTreeClassifier
 
 # Models
-X_frustrated = df[['HR_Mean', 'HR_Median', 'HR_std', 'HR_Min', 'HR_Max','HR_AUC']]
-y_frustrated = df['Frustrated']
+X_frus = df[['HR_Mean', 'HR_Median', 'HR_std', 'HR_Min', 'HR_Max','HR_AUC']]
+y_frus = df['Frustrated']
 
 # Models
-X_frus_group = df[['HR_Mean', 'HR_Median', 'HR_std', 'HR_Min', 'HR_Max','HR_AUC']]
-y_frus_group = df['Frus_Group']
+X_frus_g = df[['HR_Mean', 'HR_Median', 'HR_std', 'HR_Min', 'HR_Max','HR_AUC']]
+y_frus_g = df['Frus_Group']
 
 # Split the data into training and testing sets for Frustrated
-X_train_frustrated, X_test_frustrated, y_train_frustrated, y_test_frustrated = train_test_split(X_frustrated, y_frustrated, test_size=0.1, random_state=42)
+X_train_frus, X_test_frus, y_train_frus, y_test_frus = train_test_split(X_frus, y_frus, test_size=0.1, random_state=42)
 
 # Split the data into training and testing sets for Frus_Group
-X_train_frus_group, X_test_frus_group, y_train_frus_group, y_test_frus_group = train_test_split(X_frus_group, y_frus_group, test_size=0.1, random_state=42)
+X_train_frus_g, X_test_frus_g, y_train_frus_g, y_test_frus_g = train_test_split(X_frus_g, y_frus_g, test_size=0.1, random_state=42)
 
 # Create a logistic regression model for Frustrated
-model_frustrated = LogisticRegression(multi_class='multinomial', max_iter=1000000)
+log_model_f = LogisticRegression(multi_class='multinomial', max_iter=1000000)
 
 # Create a logistic regression model for Frus_Group
-model_frus_group = LogisticRegression(multi_class='multinomial', max_iter=1000000)
+log_model_fg = LogisticRegression(multi_class='multinomial', max_iter=1000000)
 
-baseline_frustrated = DummyClassifier(strategy='most_frequent', random_state=1)
+base_f = DummyClassifier(strategy='most_frequent', random_state=1)
 
-baseline_frus = DummyClassifier(strategy='most_frequent', random_state=1)
+base_fg = DummyClassifier(strategy='most_frequent', random_state=1)
 
-model_decision_tree_frustrated = DecisionTreeClassifier(max_depth=10)
+dt_model_f = DecisionTreeClassifier(max_depth=10)
 
-model_decision_tree_frus_group = DecisionTreeClassifier(max_depth=10)
+dt_model_fg = DecisionTreeClassifier(max_depth=10)
