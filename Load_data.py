032931d2090de20d692a5f251e0c4e814c95e6f9
Kristine <s224197@dtu.dk>
2024-01-11 14:41:11 +0100

    Noget

--- Load_data.py
+++ Load_data.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from IPython.display import display
 
 df=pd.read_csv('HR_data.csv')
 df.drop(['Unnamed: 0'],axis=1,inplace=True)
@@ -10,5 +11,3 @@ for column in columns_to_log:
 
 median_value = df['Frustrated'].median()
 df['Frus_Group'] = pd.cut(df['Frustrated'], bins=[float('-inf'), median_value, float('inf')], labels=[0, 1])
-
-df
