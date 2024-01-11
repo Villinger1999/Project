fdc75133b76b1151f72eba644d9133b45a4d8c01
Kristine <s224197@dtu.dk>
2024-01-11 13:27:34 +0100

    log

--- Load_data.py
+++ Load_data.py
@@ -3,7 +3,7 @@ import numpy as np
 
 df=pd.read_csv('HR_data.csv')
 df.drop(['Unnamed: 0'],axis=1,inplace=True)
-columns_to_log = ['HR_std', 'HR_AUC']
+columns_to_log = ['HR_std']
 
 for column in columns_to_log:
     df[column] = np.log(df[column])
