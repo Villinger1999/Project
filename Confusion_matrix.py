b069164279daa35a2309ec00b6bdef2f0ce732fa
Kristine <s224197@dtu.dk>
2024-01-12 11:32:42 +0100

    Æ

--- Confusion_matrix.py
+++ Confusion_matrix.py
@@ -2,7 +2,9 @@ from Kfold_CV import*
 from Leave_one_out import*
 from Stratified_CV import*
 
-# Confusion matrix for Frus_Group baseline
+fig, axs = plt.subplots(2, 3, figsize=(12, 8))
+
+# Stratified K-fold
 cm_log_fg_skf = metrics.confusion_matrix(y_test_fg, y_pred_fg_skf,labels=[0,1])
 cm_display_log_fg_skf = metrics.ConfusionMatrixDisplay(cm_log_fg_skf,display_labels=[0,1])
 cm_display_log_fg_skf.plot(ax=axs[1, 0])
@@ -17,6 +19,22 @@ cm_dt_fg_skf = metrics.confusion_matrix(y_test_fg, y_pred_dt_fg_skf,labels=[0,1]
 cm_display_dt_fg_skf = metrics.ConfusionMatrixDisplay(cm_dt_fg_skf,display_labels=[0,1])
 cm_display_dt_fg_skf.plot(ax=axs[1, 2])
 axs[1, 2].set_title("Decision Tree")
+
+# Confusion matrix for Frustrated
+cm_log_f_skf = metrics.confusion_matrix(y_test_f, y_pred_f_skf,labels=[0,1,2,3,4,5,6,7,8,9,10])
+cm_display_log_f_skf = metrics.ConfusionMatrixDisplay(cm_log_f_skf,display_labels=[0,1,2,3,4,5,6,7,8,9,10])
+cm_display_log_f_skf.plot(ax=axs[0, 0])
+axs[0, 0].set_title("Logistic Regression")
+
+cm_base_f_skf = metrics.confusion_matrix(y_test_f, y_pred_base_f_skf,labels=[0,1,2,3,4,5,6,7,8,9,10])
+cm_display_base_f_skf = metrics.ConfusionMatrixDisplay(cm_base_f_skf,display_labels=[0,1,2,3,4,5,6,7,8,9,10])
+cm_display_base_f_skf.plot(ax=axs[0, 1])
+axs[0, 1].set_title("Baseline")
+
+cm_dt_f_skf = metrics.confusion_matrix(y_test_f, y_pred_dt_f_skf,labels=[0,1,2,3,4,5,6,7,8,9,10])
+cm_display_dt_f_skf = metrics.ConfusionMatrixDisplay(cm_dt_f_skf,display_labels=[0,1,2,3,4,5,6,7,8,9,10])
+cm_display_dt_f_skf.plot(ax=axs[0, 2])
+axs[0, 2].set_title("Decision Tree")
 
 # Leave-one-out
 fig, axs = plt.subplots(2, 3, figsize=(12, 8))
