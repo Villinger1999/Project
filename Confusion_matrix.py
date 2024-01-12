49bea45a1c9ec514f16ba9aad8237d99e1b0416e
Kristine <s224197@dtu.dk>
2024-01-12 10:34:20 +0100

    ADD

--- Confusion_matrix.py
+++ Confusion_matrix.py
@@ -1,8 +1,11 @@
 import sklearn.metrics as metrics
 import matplotlib.pyplot as plt
 from sklearn import metrics
-<<<<<<< HEAD
 from Stratified_CV import *
+from Leave_one_out import *
+from Kfold_CV import *
+
+# Stratified K-fold
 
 fig, axs = plt.subplots(2, 3, figsize=(12, 8))
 
@@ -37,9 +40,73 @@ cm_dt_fg_skf = metrics.confusion_matrix(y_test_fg, y_pred_dt_fg_skf,labels=[0,1]
 cm_display_dt_fg_skf = metrics.ConfusionMatrixDisplay(cm_dt_fg_skf,display_labels=[0,1])
 cm_display_dt_fg_skf.plot(ax=axs[1, 2])
 axs[1, 2].set_title("Decision Tree")
-=======
-from Kfold_CV import*
-from Leave_one_out import*
-from Stratified_CV import*
->>>>>>> 46de917e66bec0f345078d0e24278b183455827c
 
+# Leave-one-out
+fig, axs = plt.subplots(2, 3, figsize=(12, 8))
+
+# Confusion matrix for Frustrated
+cm_log_f_loo = metrics.confusion_matrix(y_test_f, y_pred_f_loo,labels=[0,1,2,3,4,5,6,7,8,9,10])
+cm_display_log_f_loo = metrics.ConfusionMatrixDisplay(cm_log_f_loo,display_labels=[0,1,2,3,4,5,6,7,8,9,10])
+cm_display_log_f_loo.plot(ax=axs[0, 0])
+axs[0, 0].set_title("Logistic Regression")
+
+cm_base_f_loo = metrics.confusion_matrix(y_test_f, y_pred_base_f_loo,labels=[0,1,2,3,4,5,6,7,8,9,10])
+cm_display_base_f_loo = metrics.ConfusionMatrixDisplay(cm_base_f_loo,display_labels=[0,1,2,3,4,5,6,7,8,9,10])
+cm_display_base_f_loo.plot(ax=axs[0, 1])
+axs[0, 1].set_title("Baseline")
+
+cm_dt_f_loo = metrics.confusion_matrix(y_test_f, y_pred_dt_f_loo,labels=[0,1,2,3,4,5,6,7,8,9,10])
+cm_display_dt_f_loo = metrics.ConfusionMatrixDisplay(cm_dt_f_loo,display_labels=[0,1,2,3,4,5,6,7,8,9,10])
+cm_display_dt_f_loo.plot(ax=axs[0, 2])
+axs[0, 2].set_title("Decision Tree")
+
+# Confusion matrix for Frus_Group baseline
+cm_log_fg_loo = metrics.confusion_matrix(y_test_fg, y_pred_fg_loo,labels=[0,1])
+cm_display_log_fg_loo = metrics.ConfusionMatrixDisplay(cm_log_fg_loo,display_labels=[0,1])
+cm_display_log_fg_loo.plot(ax=axs[1, 0])
+axs[1, 0].set_title("Logistic Regression")
+
+cm_base_fg_loo = metrics.confusion_matrix(y_test_fg, y_pred_base_fg_loo,labels=[0,1])
+cm_display_base_fg_loo = metrics.ConfusionMatrixDisplay(cm_base_fg_loo,display_labels=[0,1])
+cm_display_base_fg_loo.plot(ax=axs[1, 1])
+axs[1, 1].set_title("Baseline")
+
+cm_dt_fg_loo = metrics.confusion_matrix(y_test_fg, y_pred_dt_fg_loo,labels=[0,1])
+cm_display_dt_fg_loo = metrics.ConfusionMatrixDisplay(cm_dt_fg_loo,display_labels=[0,1])
+cm_display_dt_fg_loo.plot(ax=axs[1, 2])
+axs[1, 2].set_title("Decision Tree")
+
+# K-fold
+fig, axs = plt.subplots(2, 3, figsize=(12, 8))
+
+# Confusion matrix for Frustrated
+cm_log_f_kf = metrics.confusion_matrix(y_test_f, y_pred_f_kf,labels=[0,1,2,3,4,5,6,7,8,9,10])
+cm_display_log_f_kf = metrics.ConfusionMatrixDisplay(cm_log_f_kf,display_labels=[0,1,2,3,4,5,6,7,8,9,10])
+cm_display_log_f_kf.plot(ax=axs[0, 0])
+axs[0, 0].set_title("Logistic Regression")
+
+cm_base_f_kf = metrics.confusion_matrix(y_test_f, y_pred_base_f_kf,labels=[0,1,2,3,4,5,6,7,8,9,10])
+cm_display_base_f_kf = metrics.ConfusionMatrixDisplay(cm_base_f_kf,display_labels=[0,1,2,3,4,5,6,7,8,9,10])
+cm_display_base_f_kf.plot(ax=axs[0, 1])
+axs[0, 1].set_title("Baseline")
+
+cm_dt_f_kf = metrics.confusion_matrix(y_test_f, y_pred_dt_f_kf,labels=[0,1,2,3,4,5,6,7,8,9,10])
+cm_display_dt_f_kf = metrics.ConfusionMatrixDisplay(cm_dt_f_kf,display_labels=[0,1,2,3,4,5,6,7,8,9,10])
+cm_display_dt_f_kf.plot(ax=axs[0, 2])
+axs[0, 2].set_title("Decision Tree")
+
+# Confusion matrix for Frus_Group baseline
+cm_log_fg_kf = metrics.confusion_matrix(y_test_fg, y_pred_fg_kf,labels=[0,1])
+cm_display_log_fg_kf = metrics.ConfusionMatrixDisplay(cm_log_fg_kf,display_labels=[0,1])
+cm_display_log_fg_kf.plot(ax=axs[1, 0])
+axs[1, 0].set_title("Logistic Regression")
+
+cm_base_fg_kf = metrics.confusion_matrix(y_test_fg, y_pred_base_fg_kf,labels=[0,1])
+cm_display_base_fg_kf = metrics.ConfusionMatrixDisplay(cm_base_fg_kf,display_labels=[0,1])
+cm_display_base_fg_kf.plot(ax=axs[1, 1])
+axs[1, 1].set_title("Baseline")
+
+cm_dt_fg_kf = metrics.confusion_matrix(y_test_fg, y_pred_dt_fg_kf,labels=[0,1])
+cm_display_dt_fg_kf = metrics.ConfusionMatrixDisplay(cm_dt_fg_kf,display_labels=[0,1])
+cm_display_dt_fg_kf.plot(ax=axs[1, 2])
+axs[1, 2].set_title("Decision Tree")
