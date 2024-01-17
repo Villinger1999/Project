326b0fe2493c1660603c3cf0b558d40b7d299e2c
Villinger <s224227@dtu.dk>
2024-01-17 10:59:47 +0100

    statistic + model

--- Phase/Accuracy_models_phase.py
+++ Phase/Accuracy_models_phase.py
@@ -1,18 +1,14 @@
-from Load_data_phase import *
+from ML_prepare_phase import *
 
-X = df[["HR_Mean", "HR_Median", "HR_std", "HR_Min", "HR_Max", "HR_AUC"]]
-y = df["Phase"]
+base_model_f.fit(X_train_f, y_train_f)
+dt_model_f.fit(X_train_f, y_train_f)
+log_model_f.fit(X_train_f, y_train_f)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-decisiontree = DecisionTreeClassifier()
-logistic = LogisticRegression(max_iter=1000)
-decisiontree.fit(X_train, y_train)
-logistic.fit(X_train, y_train)
-y_pred = decisiontree.predict(X_test)
-y_pred2 = logistic.predict(X_test)
-score = decisiontree.score(X_train, y_train)
-score1 = decisiontree.score(X_test, y_test)
-score4 = logistic.score(X_train, y_train)
-score5 = logistic.score(X_test, y_test)
+score = base_model_f.score(X_train_f, y_train_f)
+score1 = base_model_f.score(X_test_f, y_test_f)
+score2 = dt_model_f.score(X_train_f, y_train_f)
+score3 = dt_model_f.score(X_test_f, y_test_f)
+score4 = log_model_f.score(X_train_f, y_train_f)
+score5 = log_model_f.score(X_test_f, y_test_f)
 
-print(score, score1, score4, score5)
+print(score, score1, score2, score3, score4, score5)
--- Phase/ML_prepare_phase.py
+++ Phase/ML_prepare_phase.py
@@ -1,15 +1,15 @@
 from Load_data_phase import *
 
 # Models
-X_f = df[['HR_Mean', 'HR_Median', 'HR_std', 'HR_Min', 'HR_Max','HR_AUC']]
+X_f = df[['HR_AUC', 'Frustrated']]
 y_f = df['Phase']
 
 
 # Split the data into training and testing sets for Frustrated
-X_train_f, X_test_f, y_train_f, y_test_f = train_test_split(X_f, y_f, test_size=0.1, random_state=42)
+X_train_f, X_test_f, y_train_f, y_test_f = train_test_split(X_f, y_f, test_size=0.2, random_state=42)
 
 # Create a logistic regression model for Frustrated
-log_model_f = LogisticRegression(multi_class='multinomial', max_iter=1000000)
+log_model_f = LogisticRegression(multi_class='multinomial', max_iter=1000)
 
 base_model_f = DummyClassifier(strategy='most_frequent', random_state=1)
 
